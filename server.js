e3eabfc0a62f562fc818d152126d0e8be6557544
Fernando Guardia <fguardia321@gmail.com>
2023-01-19 18:21:59 -0300

    pagina deploy

--- server.js
+++ server.js
@@ -68,7 +68,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
-app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "../views"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
